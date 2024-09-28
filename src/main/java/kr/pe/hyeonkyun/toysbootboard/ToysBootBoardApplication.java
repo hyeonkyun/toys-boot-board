440724b887ad285297730d15e090c4b402b40a89
Hyeonkyun <thiago@naver.com>
2024-09-28 17:28:14 +0900

    최초 커밋

---
+++ src/main/java/kr/pe/hyeonkyun/toysbootboard/ToysBootBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.hyeonkyun.toysbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysBootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToysBootBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pe/hyeonkyun/toysbootboard/ToysBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.hyeonkyun.toysbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
